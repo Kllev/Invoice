ac2f05389189bed18d3d156d20bc6e8c4df68dd8
Kllev <54259977+Kllev@users.noreply.github.com>
2021-08-24 12:36:35 +0700

    Invoice

---
+++ Tugas/Tugas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RomanNumerals;
+
+namespace Tugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 10982;
+            Console.WriteLine(Generated(counter));
+
+        }
+        static string Generated(int angka)
+         {
+            angka++;
+            string getmonth, getyear,getdayNum,getdayAlp,getyear2Dig;
+            string hasil;
+            int dateNum, yearInt;
+            DateTime hariini = DateTime.Today;
+            getmonth = hariini.ToString("MM");
+            getyear = hariini.ToString("yyyy");
+            getdayNum = hariini.ToString("ddd");
+            dateNum = (int)hariini.Day;
+            getdayAlp = getdayNum.Substring(0, 2);
+            getyear2Dig = getyear.Substring(2, 2);
+            yearInt = Convert.ToInt32(getyear2Dig);
+            var getdate =  new RomanNumeral(dateNum);
+            var getyearNum = new RomanNumeral(yearInt);
+            hasil = $"INV/{getyear}{getmonth}/{getdayAlp.ToUpper()}/{getdate}/{getyearNum}/{angka}";
+            return hasil;
+         }
+    }
+}
